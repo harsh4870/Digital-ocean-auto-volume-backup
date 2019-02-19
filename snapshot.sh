037b3eed9122d1aecc1fc12cbc3a8977be9ab79e
Harsh Manvar <harsh.manvar111@gmail.com>
2019-02-19 11:54:17 +0530

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -19,6 +19,7 @@ curl -X POST \
 OLD_SNAPSHOTS=$(curl https://api.digitalocean.com/v2/snapshots?resource_type=volume \
 -H "authorization: Bearer $DIGITALOCEAN_TOKEN" -H 'cache-control: no-cache' \
 -H 'content-type: application/json' | jq ".snapshots|sort_by(.created_at)[:-2][] | .id" | sed "s/\"//g")
+## Currently, it will keep 2 newest snapshots. If you want to config this, change [2:] to other value. E.g. [1:] for keep only the newest snapshot.
 
 for SNAPSHOT_ID in $OLD_SNAPSHOTS
 do
