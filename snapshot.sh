bf282a000cf79fdf8a8d976aded1f3b06736cfa4
Harsh Manvar <harsh.manvar111@gmail.com>
2019-02-19 11:48:08 +0530

    Add files via upload

---
+++ snapshot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DIGITALOCEAN_TOKEN=''
+VOLUME_ID=''
+SNAPSHOT_NAME=''
+
+DATE=`date '+%Y%m%d-%H%M%S'`
+
+curl -X POST \
+  "https://api.digitalocean.com/v2/volumes/$VOLUME_ID/snapshots" \
+  -H "authorization: Bearer $DIGITALOCEAN_TOKEN" \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -d "{
+  \"name\": \"$SNAPSHOT_NAME-$DATE\"
+}"
+
+
+OLD_SNAPSHOTS=$(curl https://api.digitalocean.com/v2/snapshots?resource_type=volume \
+-H "authorization: Bearer $DIGITALOCEAN_TOKEN" -H 'cache-control: no-cache' \
+-H 'content-type: application/json' | jq ".snapshots|sort_by(.created_at)[:-2][] | .id" | sed "s/\"//g")
+
+for SNAPSHOT_ID in $OLD_SNAPSHOTS
+do
+    curl -X DELETE -H 'Content-Type: application/json' \
+    -H "authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/snapshots/$SNAPSHOT_ID"
+done
